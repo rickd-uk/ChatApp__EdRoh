aa2fb6758637231da1d388dc5deeec29af2be55c
Rick D <78909973+rickd-uk@users.noreply.github.com>
2023-03-10 20:57:05 +0900

    initial

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+
+function App() {
+	return (
+		<div className='App'>
+			<BrowserRouter>
+				<Routes>
+					<Route path='/chat' element={<Chat />} />
+				</Routes>
+			</BrowserRouter>
+		</div>
+	)
+}
+
+export default App
